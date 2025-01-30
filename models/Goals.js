17b71a21a7659aa1ba838125b934ee628b02b6e4
3MaheshKadam <maheshkadam9298@gmail.com>
2025-01-30 23:49:33 +0530

    created a model fro goals

---
+++ models/Goals.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const goalSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    goalType: {
+      type: String,
+      enum: ["short-term", "mid-term", "long-term"],
+      required: [true, "Goal type is required"],
+    },
+    targetDate: {
+      type: Date,
+      required: [true, "Target date is required"],
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+// Virtual fields for time tracking
+goalSchema.virtual("daysSpent").get(function () {
+  const currentDate = new Date();
+  return Math.max(
+    0,
+    Math.floor((currentDate - this.createdAt) / (1000 * 60 * 60 * 24))
+  ); // Convert milliseconds to days
+});
+
+goalSchema.virtual("totalDays").get(function () {
+  return Math.max(
+    0,
+    Math.floor((this.targetDate - this.createdAt) / (1000 * 60 * 60 * 24))
+  );
+});
+
+goalSchema.virtual("daysRemaining").get(function () {
+  const currentDate = new Date();
+  return Math.max(
+    0,
+    Math.floor((this.targetDate - currentDate) / (1000 * 60 * 60 * 24))
+  );
+});
+
+goalSchema.virtual("timeProgress").get(function () {
+  return this.totalDays > 0
+    ? Math.min((this.daysSpent / this.totalDays) * 100, 100)
+    : 0;
+});
+
+export default mongoose.model("Goal", goalSchema);
